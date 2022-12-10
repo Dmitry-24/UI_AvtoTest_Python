fd3fda10a10a640842479fad0637013f10fb3c2f
dmitr <Dmitry-perfilev94@yande.ru>
2022-12-10 15:13:36 +0300

    added marks and update test can add product to basket

--- test_product_page.py
+++ test_product_page.py
@@ -1,13 +1,13 @@
 from pages.product_page import ProductPage
 import time
-from selenium.webdriver.support.ui import WebDriverWait as EC
+import pytest
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('promo_offer', ["0","1", "3", "4", "5", "6", pytest.param("bugged_link", marks=pytest.mark.xfail), "8", "9"])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     page = ProductPage(browser, link)
     page.open()
     page.get_to_backet()
     page.solve_quiz_and_get_code()
-    page.should_be_true_price()
     page.should_be_true_item()
 
